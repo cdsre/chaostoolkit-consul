b9ea0ef454e01c5d79d2c13d79de4f887d521995
Chris Doyle <uk.chris.doyle+cdsre@gmail.com>
2024-04-12 10:37:44 +0100

    chore: updated code to fix linting and mypy errors
    Signed-off-by: Chris Doyle <uk.chris.doyle+cdsre@gmail.com>

--- chaosconsul/kv/probes.py
+++ chaosconsul/kv/probes.py
@@ -1,6 +1,5 @@
 # -*- coding: utf-8 -*-
 from chaoslib.exceptions import FailedActivity
-from chaoslib.types import Configuration, Secrets
 from consul import Consul
 
 __all__ = ["key_exists", "get_value_for_key"]
@@ -24,5 +23,7 @@ def get_value_for_key(key: str) -> str:
 
     index, data = client.kv.get(key)
     if data is None:
-        raise FailedActivity(f"Key '{key}' does not exist in the Consul KV store")
-    return data["Value"].decode("utf-8")
+        raise FailedActivity(
+            f"Key '{key}' does not exist in the Consul KV store"
+        )
+    return bytes(data["Value"]).decode("utf-8")
